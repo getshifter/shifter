cd98bff3a0392e990f03194628797d49605385c3
Daniel Olson <emaildano@gmail.com>
2016-11-16 16:57:19 -0500

    header checkin

--- app/themes/shifter/templates/global/header.php
+++ app/themes/shifter/templates/global/header.php
@@ -2,13 +2,19 @@
   use Apollo\Extend\Util;
 ?>
 
-<header class="navigation" role="banner">
-  <div class="container">
-    <a href="<?= esc_url(home_url('/')); ?>" class="logo">
-      <img class="image" width="75" height="auto" src="<?= get_template_directory_uri();?>/assets/images/shifter-logo--white.svg" alt="<?php bloginfo('name'); ?>">
-    </a>
-    <nav role="navigation">
+<header class="global-header" role="banner">
+  <div class="container-flex">
+    <div class="logo">
+      <a href="<?= esc_url(home_url('/')); ?>">
+        <img width="130" height="auto" src="<?= get_template_directory_uri();?>/assets/images/shifter-logo--white.svg" alt="<?php bloginfo('name'); ?>">
+      </a>
+    </div>
+    <nav class="global-nav" role="navigation">
       <?= Util\Listless_WP_Nav('primary_navigation'); ?>
+      <div class="signup-links">
+        <a class="btn-outline--white" href="#">Login</a>
+        <a class="btn-outline--white" href="#">Register</a>
+      </div>
     </nav>
   </div>
 </header>
