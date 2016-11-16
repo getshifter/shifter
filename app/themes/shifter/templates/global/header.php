f06f53dfa6abf8b5a8ffc0112bff2dea80a34db2
Daniel Olson <emaildano@gmail.com>
2016-11-16 14:37:58 -0500

    type styles

--- app/themes/shifter/templates/global/header.php
+++ app/themes/shifter/templates/global/header.php
@@ -5,26 +5,10 @@
 <header class="navigation" role="banner">
   <div class="container">
     <a href="<?= esc_url(home_url('/')); ?>" class="logo">
-      <img class="image" width="75" height="auto" src="<?= get_template_directory_uri();?>/assets/images/logo-image.svg" alt="<?php bloginfo('name'); ?>">
+      <img class="image" width="75" height="auto" src="<?= get_template_directory_uri();?>/assets/images/shifter-logo--white.svg" alt="<?php bloginfo('name'); ?>">
     </a>
     <nav role="navigation">
-      <?php
-
-        echo Util\Listless_WP_Nav('primary_navigation');
-
-        // if (has_nav_menu('primary_navigation')) :
-        //   echo '<ul class="navigation-menu">';
-        //     $primary_nav = array(
-        //       'theme_location' => 'primary_navigation',
-        //       'depth' => 3,
-        //       'menu_class' => '',
-        //       'items_wrap'=>'%3$s',
-        //       'container' => false
-        //     );
-        //     wp_nav_menu($primary_nav);
-        //   echo '</ul>';
-        // endif;
-      ?>
+      <?= Util\Listless_WP_Nav('primary_navigation'); ?>
     </nav>
   </div>
 </header>
