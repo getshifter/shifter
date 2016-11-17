13a1c953a3cba428ea4961c8ba0cfd7ec0ecada8
Shaun Baer <shaun.baer@gmail.com>
2016-11-17 09:26:55 -0500

    shaun

--- app/themes/shifter/assets/js/modules/commonScripts.js
+++ app/themes/shifter/assets/js/modules/commonScripts.js
@@ -6,4 +6,8 @@ module.exports = function() {
 
   console.log('We have liftoff.');
 
+  require('./node_modules/gsap/src/uncompressed/jquery.gsap.js');
+  require('./node_modules/gsap/src/uncompressed/TweenLite.js');
+  require('scrollmagic');
+
 };
