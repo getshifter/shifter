1a67c1ff9c8c984ffdd9560123e913ee7145ae76
Daniel Olson <emaildano@gmail.com>
2016-11-18 18:11:02 -0500

    global signup

---
+++ app/themes/shifter/templates/global/signup.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Global: Sign up
+ */
+?>
+
+<section class="_white section-padding">
+  <div class="container">
+    <article class="global-signup">
+      <div class="global-signup__title">
+        <h1 class="md-sans black"><span>Are you ready for the next big thing?</span><br>Get started with Shifter today!</h1>
+      </div>
+      <a href="#" class="btn-outline--purple">Sign up</a>
+    </article>
+  </div>
+</section>
