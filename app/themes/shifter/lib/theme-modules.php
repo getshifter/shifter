e90275baee6901662859d9495e9630491fab6847
Daniel Olson <emaildano@gmail.com>
2016-11-27 16:39:53 -0500

    updated blog link

--- app/themes/shifter/lib/theme-modules.php
+++ app/themes/shifter/lib/theme-modules.php
@@ -23,15 +23,16 @@ function post_module() {
       </figure>
       <div class="blog__content">
         <header>
-          <h2 class="md-sans blog__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h2 class="md-sans blog__title"><a class="black" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <div class="blog__meta sm-caps xs-sans">
             <time datetime="<?= get_the_time('c'); ?>"><?= get_the_date(); ?></time>
             <a href="#">#Blog</a>
           </div>
         </header>
-        <div class="entry-summary">
+        <div class="entry-summary xs-sans">
           <?php the_excerpt(); ?>
         </div>
+        <a class="sm-caps blog__link" href="<?= the_permalink() ?>">Read More &rsaquo;</a>
       </div>
     </article>
   <?php
