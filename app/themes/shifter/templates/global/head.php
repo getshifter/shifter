7fbaacfe342157196863d02d36dd8f2c2d8bd541
Daniel Olson <emaildano@gmail.com>
2016-11-30 15:04:55 -0800

    updated favicon

--- app/themes/shifter/templates/global/head.php
+++ app/themes/shifter/templates/global/head.php
@@ -11,8 +11,6 @@
   <!-- Remove .no-js class if possible -->
   <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
 
-  <!--[if IE]><link rel="shortcut icon" href="<?= $icon_path ?>favicon.ico"><![endif]-->
-  <link rel="apple-touch-icon-precomposed" href="<?= $icon_path ?>favicon.icns">
   <link rel="icon" href="<?= $icon_path ?>favicon.png">
 
   <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
