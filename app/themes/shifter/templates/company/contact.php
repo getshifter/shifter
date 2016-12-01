fa349ed76fe34a9dd75eedfc9ef9de558aa8cb3a
Daniel Olson <emaildano@gmail.com>
2016-11-30 23:50:12 -0800

    updated contact form style

--- app/themes/shifter/templates/company/contact.php
+++ app/themes/shifter/templates/company/contact.php
@@ -12,7 +12,7 @@
     <section class="contact">
 
       <div class="contact__form">
-        <?php echo do_shortcode( '[contact-form-7 id="106" title="Support Form"]' ); ?>
+        <?php echo do_shortcode( '[contact-form-7 id="70" title="Support Form"]' ); ?>
       </div>
 
     </section>
