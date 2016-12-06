b7afc6ec4fe53b856e156330e28a10641b5033e4
Daniel Olson <emaildano@gmail.com>
2016-12-06 18:27:57 -0500

    updated copy

--- app/themes/shifter/templates/front-page/overview.php
+++ app/themes/shifter/templates/front-page/overview.php
@@ -34,16 +34,16 @@
           <img class="img-r" width="280" height="auto" src="<?= get_template_directory_uri();?>/dist/images/dashboard-icon.svg" alt="">
         </div>
         <div class="overview__content">
-          <h2 class="md-sans overview__title">Serverless Architecture</h2>
+          <h2 class="md-sans overview__title">Built for WordPress</h2>
           <p>
-            Static web pages are nearly maintenance-free and load at lightening speed. But they require outside development resources and don’t offer the functionality of a dynamic web page.
+            WordPress makes building a website easy and cost effective for almost any user. But it needs regular updates and security can be a concern.
           </p>
           <div class="overview__highlights">
             <h3 class="xs-sans sm-caps purple">Highlights</h3>
             <ul class="item list-unstyled--seperators">
-              <li>Speed</li>
-              <li>Security</li>
-              <li>Scalability</li>
+              <li>Ease of use</li>
+              <li>Functionality</li>
+              <li>Design</li>
             </ul>
           </div>
         </div>
