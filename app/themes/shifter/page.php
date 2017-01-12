6054d3091384260d8f7eea00761e2884b22a3b06
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-12 23:58:59 +0900

    setup default page template ( same as single.php )

--- app/themes/shifter/page.php
+++ app/themes/shifter/page.php
@@ -1,15 +1,18 @@
 <?php while (have_posts()) : the_post(); ?>
-  <div class="container">
-    <article <?php post_class('single__article _white'); ?>>
-      <div class="single__content">
-        <div class="entry-content">
-          <?php
-            // https://github.com/j2made/acf-type
-            // get_template_part('templates/type/_type-main');
-            the_content();
-          ?>
+  <div class="_gradient section-padding">
+    <div class="container">
+      <article <?php post_class('single__article _white'); ?>>
+        <div class="single__content">
+          <div class="entry-content">
+            <?php
+              // https://github.com/j2made/acf-type
+              // get_template_part('templates/type/_type-main');
+              the_content();
+            ?>
+          </div>
         </div>
       </div>
     </article>
   </div>
+  <?php get_template_part('templates/global/signup'); ?>
 <?php endwhile; ?>
