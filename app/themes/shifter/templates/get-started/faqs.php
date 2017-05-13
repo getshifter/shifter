2c63659916d3a2af59712e834386de269afc89f3
Daniel Olson <emaildano@gmail.com>
2017-05-13 11:31:07 -0400

    updated FAQs banner

--- app/themes/shifter/templates/get-started/faqs.php
+++ app/themes/shifter/templates/get-started/faqs.php
@@ -7,7 +7,7 @@ namespace Shifter\FAQs;
 use Shifter\Theme\ZendeskAPI;
 ?>
 
-<section class="_gray-light section-padding">
+<!-- <section class="_gray-light section-padding">
   <div class="container">
     <article class="faqs">
       <header class="faqs__lead section-lead">
@@ -16,6 +16,18 @@ use Shifter\Theme\ZendeskAPI;
       <section class="accordion__list faqs__list">
         <?php ZendeskAPI\get_zendesk_articles(); ?>
       </section>
+    </article>
+  </div>
+</section> -->
+
+
+<section class="_gray-light section-padding">
+  <div class="container">
+    <article class="global-signup">
+      <div class="global-signup__title">
+        <h1 class="md-sans black"><span>Shifter Documentation</span><br> Guides, FAQs, Shifter API, and More</h1>
+      </div>
+      <a href="https://docs.getshifter.io/" class="btn-outline--purple">Shifter Docs</a>
     </article>
   </div>
 </section>
